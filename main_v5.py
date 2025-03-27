85aa7ea63c01ff00fe4f4fcefef2dfe0af23c5b4
haru <harukayoshida25de@gmail.com>
2025-03-27 18:35:37 +0900

    main5

--- main_v5.py
+++ main_v5.py
@@ -10,7 +10,8 @@ client = openai.OpenAI(api_key=st.secrets["GPTAPI"].get("OPENAI_API_KEY"))
 # Google Books APIキーの取得
 # books_api_key = st.secrets["google"]["books_api_key"]
 st.write(st.secrets)  # secretsの中身を確認
-books_api_key = st.secrets["google"]["books_api_key
+books_api_key = st.secrets["google"]["books_api_key"]
+
 
 # タイトル
 st.title('📚 学びたい内容に合った本をおすすめ！')
